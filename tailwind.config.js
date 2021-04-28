75ccff49561c7db05965ae54155d6ff035143b7a
Serge Bazunoff <sergey.bazunoff@gmail.com>
2021-04-28 19:20:27 +0300

    icd-suche top panels

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,23 @@ module.exports = {
     'index.html'
   ],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+  theme: {   
+    fontFamily: {
+      'sans': ['Segoe UI', 'Arial'],
+      'serif': ['Times New Roman'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['Verdana'],
+     }, 
+    extend: {      
+      colors: {
+        blue: {
+          light: '#85d7ff',
+          DEFAULT: '#337ab7',
+          dark: '#009eeb',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
